0cfdbb3ba6e480da754dfca39cf2e89ad83d06b2
y011d4 <yoiida333@gmail.com>
2023-01-17 12:23:15 +0900

    Auto ls in fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,15 +5,20 @@ end
 set -U FZF_LEGACY_KEYBINDINGS 0
 
 function __ghqpeco -d "List git repositories"
-    # 必要になったらLBUFFERを定義
-    set -l LBUFFER ""
-    set -l selected_dir $(ghq list -p | peco --query "$LBUFFER")
-    echo $selected_dir
-    if test $selected_dir:
-        cd $selected_dir
-    end
+  # 必要になったらLBUFFERを定義
+  set -l LBUFFER ""
+  set -l selected_dir $(ghq list -p | peco --query "$LBUFFER")
+  echo $selected_dir
+  if test $selected_dir:
+      cd $selected_dir
+  end
 end
 bind \cg '__ghqpeco'
+
+functions --copy cd original_cd
+function cd
+  original_cd $argv; and ls
+end
 
 set -g theme_color_scheme dark
 
