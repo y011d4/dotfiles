5c7da13188b1deca7abe536b07d753abb8cf2be8
y011d4 <yoiida333@gmail.com>
2023-01-19 00:33:09 +0900

    update pyenv

--- .venv/python3.10.9/install.fish
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/fish
-pyenv init - | source
-pyenv local 3.10.9
-python -m venv ~/.virtualenvs/3.10.9
-source ~/.virtualenvs/3.10.9/bin/activate.fish
-poetry export -f requirements.txt > requirements.txt
-pip3 install -r requirements.txt
-poetry install
---
+++ .venv/python3.10.9/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+python_version="3.10.9"
+
+eval "$(pyenv init --path)"
+
+pyenv local $python_version
+if [ $? -ne 0 ]; then
+  pyenv install $python_version
+  pyenv local $python_version
+fi
+
+python -m venv "$HOME/.virtualenvs/$python_version"
+source "$HOME/.virtualenvs/$python_version/bin/activate"
+poetry export -f requirements.txt > requirements.txt
+pip3 install -r requirements.txt
+poetry install
