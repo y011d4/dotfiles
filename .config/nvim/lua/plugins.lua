a895e58e9d4ef753614eebcca3d4749e06eb0236
y011d4 <yoiida333@gmail.com>
2023-01-19 19:35:26 +0900

    Add noice

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -327,7 +327,7 @@ require("lazy").setup({
       require("nvim-cmp-setting")
     end
   },
-  { 
+  {
     'hrsh7th/cmp-nvim-lsp',
     lazy = true,
     event = "InsertEnter",
@@ -451,8 +451,10 @@ require("lazy").setup({
     'lewis6991/gitsigns.nvim',
     dependencies = {
       "petertriho/nvim-scrollbar",
+      'folke/trouble.nvim',
     },
-    lazy = false,
+    lazy = true,
+    event = { "FocusLost", "CursorHold" },
     config = function()
       require("gitsigns").setup {}
       require("scrollbar.handlers.gitsigns").setup()
@@ -566,4 +568,41 @@ require("lazy").setup({
       'y011d4/null-ls.nvim',
     }
   },
+  {
+    "folke/noice.nvim",
+    lazy = false,
+    event = { "BufRead", "BufNewFile", "InsertEnter", "CmdlineEnter" },
+    config = function()
+      require("noice").setup({
+        lsp = {
+          -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+          override = {
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.stylize_markdown"] = true,
+            ["cmp.entry.get_documentation"] = true,
+          },
+          signature = {
+            enabled = false,
+          },
+        },
+        -- you can enable a preset for easier configuration
+        presets = {
+          bottom_search = true, -- use a classic bottom cmdline for search
+          command_palette = true, -- position the cmdline and popupmenu together
+          long_message_to_split = true, -- long messages will be sent to a split
+          inc_rename = false, -- enables an input dialog for inc-rename.nvim
+          lsp_doc_border = false, -- add a border to hover docs and signature help
+        },
+        -- add any options here
+      })
+    end,
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      "rcarriga/nvim-notify",
+    }
+  }
 })
