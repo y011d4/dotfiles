6cd05599564ec24a1c55dd633721669b8477f459
y011d4 <yoiida333@gmail.com>
2023-01-20 21:55:12 +0900

    ufo

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -687,4 +687,35 @@ require("lazy").setup({
       require("dap-python").setup("~/.virtualenvs/debugpy/bin/python")
     end,
   },
+  -- 下のやつで使うので
+  {
+    "kevinhwang91/promise-async",
+    lazy = true,
+  },
+  -- unfold, fold
+  {
+    "kevinhwang91/nvim-ufo",
+    dependencies = {
+      "kevinhwang91/promise-async",
+    },
+    lazy = true,
+    event = "BufRead",
+    config = function()
+      vim.o.foldcolumn = "1" -- '0' is not bad
+      vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+      vim.o.foldlevelstart = 99
+      vim.o.foldenable = true
+      -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
+      vim.keymap.set("n", "zR", require("ufo").openAllFolds)
+      vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
+      -- Option 3: treesitter as a main provider instead
+      -- Only depend on `nvim-treesitter/queries/filetype/folds.scm`,
+      -- performance and stability are better than `foldmethod=nvim_treesitter#foldexpr()`
+      require("ufo").setup({
+        provider_selector = function(bufnr, filetype, buftype)
+          return { "treesitter", "indent" }
+        end,
+      })
+    end,
+  },
 })
