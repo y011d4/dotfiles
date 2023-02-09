7e4da06262f2520488d7c7a9a5f95bb5a3289983
y011d4 <yoiida333@gmail.com>
2023-02-09 14:23:17 +0900

    update python env

--- .venv/python3.10.9/install.sh
+++ .venv/python3.10.9/install.sh
@@ -2,16 +2,17 @@
 
 python_version="3.10.9"
 
-eval "$(pyenv init --path)"
-
-pyenv local $python_version
-if [ $? -ne 0 ]; then
-  pyenv install $python_version
+if [ ! -e "$HOME/.virtualenvs/$python_version" ]; then
+  eval "$(pyenv init --path)"
   pyenv local $python_version
+  if [ $? -ne 0 ]; then
+    pyenv install $python_version
+    pyenv local $python_version
+  fi
+  python3 -m venv "$HOME/.virtualenvs/$python_version"
 fi
-
-python3 -m venv "$HOME/.virtualenvs/$python_version"
 source "$HOME/.virtualenvs/$python_version/bin/activate"
+poetry lock
 poetry export -f requirements.txt > requirements.txt
 pip3 install -r requirements.txt
 poetry install
